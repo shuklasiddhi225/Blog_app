56c861d0bc3dc020986cd88ac73bf5270f800e35
shuklasiddhi225 <shuklasiddhi225@gmail.com>
2025-05-22 22:23:39 +0530

    middleware andd error handler done

--- api/index.js
+++ api/index.js
@@ -1,22 +1,39 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
-mongoose.connect
-(process.env.MONGO)
-.then(
-    ()=>{console.log('MongoDB is connected')}
-).catch(err =>
-{ 
-    console.log(err);
-}
-);
-const app=express(); 
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+const app = express();
+
 app.use(express.json());
-app.listen(3000,() =>{
-    console.log('Server is running on  port 3000!');
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
 });
-app.use('/api/user',userRoutes);
-app.use('/api/auth',authRoutes);
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
